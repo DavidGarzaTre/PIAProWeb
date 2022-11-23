2899d737ab99c83e70d5953d3687cb1b52f99d65
DavidGarza250 <111547599+DavidGarza250@users.noreply.github.com>
2022-11-23 17:58:11 -0600

    Se agrego la pantalla de los productos y las categorias de productos

--- PIAProWeb/Controllers/HomeController.cs
+++ PIAProWeb/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PIAProWeb.Models;
+using PIAProWeb.Models.dbModels;
 using System.Diagnostics;
 
 namespace PIAProWeb.Controllers
@@ -7,10 +8,14 @@ namespace PIAProWeb.Controllers
     public class HomeController : Controller
     {
         private readonly ILogger<HomeController> _logger;
+        private readonly PIAProWebContext _context;
+        private readonly IWebHostEnvironment _webHostEnvironment;
 
-        public HomeController(ILogger<HomeController> logger)
+        public HomeController(PIAProWebContext context, IWebHostEnvironment webHostEnvironment, ILogger<HomeController> logger)
         {
             _logger = logger;
+            _context = context;
+            _webHostEnvironment = webHostEnvironment;
         }
 
         public IActionResult Index()
@@ -21,6 +26,22 @@ namespace PIAProWeb.Controllers
         public IActionResult Privacy()
         {
             return View();
+        }
+
+        [HttpGet("[controller]/Componentes/{IdCategoria?}")]
+        public IActionResult Componentes(int? IdCategoria =null)
+        {
+            List<Producto> ListaProducto = null;
+            if(IdCategoria == null)
+            {
+                ListaProducto = _context.Productos.ToList();
+            }
+            else
+            {
+                ListaProducto = _context.Productos.Where(x=>x.IdCategoria== IdCategoria).ToList();
+            }
+
+            return View(ListaProducto);
         }
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
