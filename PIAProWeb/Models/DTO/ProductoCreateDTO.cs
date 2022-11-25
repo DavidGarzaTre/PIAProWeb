8c9306beb62019fb6b63db0f90490a2384aa0945
DavidGarza250 <111547599+DavidGarza250@users.noreply.github.com>
2022-11-25 12:10:39 -0600

    Se modifico la pantalla index del controlaodr home y se aplcaron otros estilos css

--- PIAProWeb/Models/DTO/ProductoCreateDTO.cs
+++ PIAProWeb/Models/DTO/ProductoCreateDTO.cs
@@ -15,5 +15,40 @@ namespace PIAProWeb.Models.DTO
         public IFormFile? ImagenProducto { get; set; }=null!;
         [StringLength(50)]
         public string StockProducto { get; set; } = null!;
+        [StringLength(50)]
+
+        //SpecsCPU
+        public string Marca { get; set; } = null!;
+        [StringLength(50)]
+        public string Modelo { get; set; } = null!;
+        [StringLength(50)]
+        public string Arquitectura { get; set; } = null!;
+        [StringLength(50)]
+        public string Generacion { get; set; } = null!;
+        [StringLength(50)]
+        public string Velocidad { get; set; } = null!;
+        [StringLength(50)]
+        public string Socket { get; set; } = null!;
+        [StringLength(50)]
+        public string Nucleos { get; set; } = null!;
+        [Column("Grafico_Integrados")]
+        [StringLength(50)]
+        public string? GraficoIntegrados { get; set; }
+        [Column("Frecuencia_de_Graficos")]
+        [StringLength(50)]
+        public string? FrecuenciaDeGraficos { get; set; }
+        [Column("TDP")]
+        public int Tdp { get; set; }
+        [StringLength(50)]
+        public string? Disipador { get; set; }
+
+        //Specs Disco Duro
+        [Column("Tipo_Disco")]
+        [StringLength(100)]
+        public string TipoDisco { get; set; } = null!;
+        [StringLength(50)]
+        public string Interfaz { get; set; } = null!;
+        [StringLength(50)]
+        public string Capacidad { get; set; } = null!;
     }
 }
